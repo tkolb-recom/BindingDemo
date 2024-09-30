c09e625940c0495e3b8f52a60f976b4cee5135cd
Tobias Kolb <tkolb@recom.eu>
2024-09-30 13:49:06 +0200

    complex example (data grid, editing)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,24 +33,27 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-            this.textSourceBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.parentSourceBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.textDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textSourceBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.parentSourceBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.textSourceBindingSource, "Text", true));
-            this.textBox1.Location = new System.Drawing.Point(25, 33);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bindingSource1, "Text", true));
+            this.textBox1.Location = new System.Drawing.Point(12, 240);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(229, 20);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 59);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 266);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -60,38 +63,60 @@
             // 
             // listBox1
             // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(25, 103);
+            this.listBox1.Location = new System.Drawing.Point(284, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(229, 134);
+            this.listBox1.Size = new System.Drawing.Size(229, 277);
             this.listBox1.TabIndex = 2;
             // 
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
-            this.errorProvider1.DataSource = this.textSourceBindingSource;
+            this.errorProvider1.DataSource = this.bindingSource1;
             // 
-            // textSourceBindingSource
+            // dataGridView1
             // 
-            this.textSourceBindingSource.DataSource = typeof(BindingDemo.TextSource);
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.textDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.bindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(266, 222);
+            this.dataGridView1.TabIndex = 3;
             // 
-            // parentSourceBindingSource
+            // bindingSource1
             // 
-            this.parentSourceBindingSource.DataSource = typeof(BindingDemo.ParentSource);
+            this.bindingSource1.AllowNew = false;
+            this.bindingSource1.DataSource = typeof(BindingDemo.TextSource);
+            // 
+            // textDataGridViewTextBoxColumn
+            // 
+            this.textDataGridViewTextBoxColumn.DataPropertyName = "Text";
+            this.textDataGridViewTextBoxColumn.HeaderText = "Text";
+            this.textDataGridViewTextBoxColumn.Name = "textDataGridViewTextBoxColumn";
+            this.textDataGridViewTextBoxColumn.Width = 200;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(525, 306);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "How to binding";
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textSourceBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.parentSourceBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,11 +125,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.BindingSource textSourceBindingSource;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ErrorProvider errorProvider1;
-        private System.Windows.Forms.BindingSource parentSourceBindingSource;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn textDataGridViewTextBoxColumn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BindingDemo
@@ -16,21 +10,30 @@ namespace BindingDemo
         {
             InitializeComponent();
 
-            var ps = new ParentSource();
-            ps.PropertyChanged += PropertyChangedHandler;
-            ps.Nested.PropertyChanged += PropertyChangedHandler;
-            textSourceBindingSource.DataSource = ps.Nested;
+            bindingSource1.AddingNew += (s, ev) => Write("AddingNew");
+            bindingSource1.BindingComplete += (s, ev) => Write("BindingComplete");
+            bindingSource1.CurrentChanged += (s, ev) => Write("CurrentChanged");
+            bindingSource1.CurrentItemChanged += (s, ev) => Write("CurrentItemChanged");
+            bindingSource1.DataError += (s, ev) => Write("DataError");
+            bindingSource1.DataMemberChanged += (s, ev) => Write("DataMemberChanged");
+            bindingSource1.DataSourceChanged += (s, ev) => Write("DataSourceChanged");
+            bindingSource1.ListChanged += (s, ev) => Write("ListChanged");
+            bindingSource1.PositionChanged += (s, ev) => Write("PositionChanged");
+
+            bindingSource1.Add(new TextSource { Text = "John" });
+            bindingSource1.Add(new TextSource { Text = "Jill" });
+            bindingSource1.Add(new TextSource { Text = "Jack" });
+            bindingSource1.Add(new TextSource { Text = "Joan" });
         }
 
-        private void PropertyChangedHandler(object sender, PropertyChangedEventArgs e)
+        private void Write(string text)
         {
-            listBox1.Items.Add($"{DateTime.Now.ToLongTimeString()} {sender.GetType().Name}.{e.PropertyName}");
+            listBox1.Items.Add($"{DateTime.Now.ToLongTimeString()} {text}");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string text = ((TextSource)textSourceBindingSource.DataSource).Text;
-            MessageBox.Show(text);
+            MessageBox.Show(((TextSource)bindingSource1.Current).Text);
         }
     }
 }
